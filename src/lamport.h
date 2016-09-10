ff49ba6a05e1afe52b65e52c0d1d084363c20b32
Alonzo Coueus <alonzo.coeus@mail.com>
2016-09-10 18:14:15 +0100

    Update lamport.h

--- src/lamport.h
+++ src/lamport.h
@@ -1,1 +1,28 @@
+// Copyright (c) 2016 Alonzo-Coeus
+#ifndef LAMPORT_LAMPORT_H
+#define LAMPORT_LAMPORT_H
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+#include "crypto/ripemd160.h"
+
+using namespace boost::multiprecision;
+typedef std::vector<unsigned char> valtype;
+
+class LAMPORT
+{
+private:
+    char pubkeys[320][20];
+    char prikeys[320][20];
+    char sig[20][2][20];
+public:
+    static const int chunksize = 1; /* in bytes */
+    bool checksig(valtype *pointerdata, valtype *pointerasig, valtype *pointerrootkey, valtype *pointermerklewit);
+    unsigned char *createsig(valtype *pdata, uint512_t *pprikey, int sellectedpubkey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
+    unsigned char *createmerklewit(uint512_t prikey, int sellectedpubkey);
+    unsigned char *createrootkey(uint512_t prikey);
+    unsigned char *createpubkey(uint512_t prikey);
+};
+
+#endif // LAMPORT_LAMPORT_H
 
